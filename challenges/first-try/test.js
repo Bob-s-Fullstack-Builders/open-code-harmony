756b5fd5c26bcf247dfcea908a743a00baff737d
Jim Nelson <jim@jimnelson.us>
2022-06-08 21:50:09 -0700

    added license

--- challenges/first-try/test.js
+++ challenges/first-try/test.js
@@ -1,9 +1,13 @@
-const assert = require('assert');
+// Enclosing in IIFE to reduce chances of name collisions
+//  when concatenating tests to submission
+(function () {
+    const assert = require('assert');
 
-describe('First Try', function () {
-    describe('one()', function () {
-        it('should return 1 when called', function () {
-            assert.equal(one(), 1);
+    describe('First Try', function () {
+        describe('one()', function () {
+            it('should return 1 when called', function () {
+                assert.equal(one(), 1);
+            });
         });
     });
-});
+})()
